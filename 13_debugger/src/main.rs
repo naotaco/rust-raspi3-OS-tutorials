54b3b934a85086a78593bd33c45cba357ce44b4f
Nao Taco <naotaco@gmail.com>
2019-02-27 23:22:39 +0900

    cleanup

--- 13_debugger/src/main.rs
+++ 13_debugger/src/main.rs
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2018 Andre Richter <andre.o.richter@gmail.com>
+ * Copyright (c) 2019 Nao Taco <naotaco@gmail.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -28,73 +28,15 @@
 
 const MMIO_BASE: u32 = 0x3F00_0000;
 
-mod arm_debug;
-mod gpio;
-mod mbox;
-mod uart;
-
-use core::sync::atomic::{compiler_fence, Ordering};
+mod debugger;
 
 fn kernel_entry() -> ! {
-    let mut mbox = mbox::Mbox::new();
-    let uart = uart::Uart::new();
+    // To change some GPIO pins to ARM debugger function.
+    // After this point, your debugger can attach to the CPU.
+    debugger::setup_debug();
 
-    // set up serial console
-    match uart.init(&mut mbox) {
-        Ok(_) => uart.puts("\n[0] UART is live!\n"),
-        Err(_) => loop {
-            unsafe { asm!("wfe" :::: "volatile") }; // If UART fails, abort early
-        },
-    }
-
-    uart.puts("[1] Press a key to continue booting... ");
-    uart.getc();
-    uart.puts("Greetings fellow Rustacean!\n");
-
-    // get the board's unique serial number with a mailbox call
-    mbox.buffer[0] = 8 * 4; // length of the message
-    mbox.buffer[1] = mbox::REQUEST; // this is a request message
-    mbox.buffer[2] = mbox::tag::GETSERIAL; // get serial number command
-    mbox.buffer[3] = 8; // buffer size
-    mbox.buffer[4] = 8;
-    mbox.buffer[5] = 0; // clear output buffer
-    mbox.buffer[6] = 0;
-    mbox.buffer[7] = mbox::tag::LAST;
-
-    // Insert a compiler fence that ensures that all stores to the
-    // mbox buffer are finished before the GPU is signaled (which is
-    // done by a store operation as well).
-    compiler_fence(Ordering::Release);
-
-    // send the message to the GPU and receive answer
-    let serial_avail = match mbox.call(mbox::channel::PROP) {
-        Err(_) => false,
-        Ok(()) => true,
-    };
-
-    if serial_avail {
-        uart.puts("[i] My serial number is: 0x");
-        uart.hex(mbox.buffer[6]);
-        uart.hex(mbox.buffer[5]);
-        uart.puts("\n");
-    } else {
-        uart.puts("[i] Unable to query serial!\n");
-    }
-
-    arm_debug::setup_debug();
-
-    let mut counter = 0;
-    // echo everything back
-    loop {
-        counter += 1;
-        match counter % 0x10000 == 0 {
-            false => (),
-            true => {
-                uart.hex(counter);
-                uart.puts("\n");
-            }
-        }
-    }
+    // Wait to be attached.
+    loop {}
 }
 
 raspi3_boot::entry!(kernel_entry);
